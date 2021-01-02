207cf90738d1a600a5ce82c2c4d8ddabaea3150c
Richard Morris <rwmorris.bjj@gmail.com>
2021-01-02 18:36:44 +0000

    updating file

--- JS210_small_problems/evennumbers.js
+++
@@ -1,5 +0,0 @@
-let index = 2
-while (index < 99) {
-  console.log(index)
-  index += 2
-}
--- JS210_small_problems/oddnumbers.js
+++
@@ -1,5 +0,0 @@
-// Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
-
-for(let index = 1; index < 100; index += 2) {
-  console.log(index)
-}
