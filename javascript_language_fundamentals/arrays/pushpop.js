c77f3dfedd738c05da0be292c9a295f97f711962
Richard Morris <rwmorris.bjj@gmail.com>
2021-01-08 20:43:29 +0000

    adding practise problems

---
+++ javascript_language_fundamentals/arrays/pushpop.js
@@ -0,0 +1,32 @@
+// In this exercise, you will implement your own version of two Array methods: Array.prototype.pop and Array.prototype.push. 
+// The pop method removes the last element from an array and returns that element. If pop is called on an empty array, 
+// it returns undefined. The push method, on the other hand, adds one or more elements to the end of an array and 
+// returns the new length of the array.
+
+// Examples:
+// pop
+
+function pop(array) {
+  if (array.length === 0) {
+    return undefined
+  }
+
+  let lastElement = array[array.length -1]
+
+  array.length = array.length - 1
+
+  return lastElement
+}
+const array1 = [1, 2, 3];
+console.log(pop(array1));                        // 3
+console.log(array1);                // [1, 2]
+console.log(pop([]));                           // undefined
+console.log(pop([1, 2, ['a', 'b', 'c']]));      // ["a", "b", "c"]
+
+// // push
+// const array2 = [1, 2, 3];
+// push(array2, 4, 5, 6);              // 6
+// console.log(array2);                // [1, 2, 3, 4, 5, 6]
+// push([1, 2], ['a', 'b']);          // 3
+// push([], 1);                       // 1
+// push([]);                          // 0
