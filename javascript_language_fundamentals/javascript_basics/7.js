6cb3a62cd9ed4bb54a5ab05bb6b0790c0f970144
Richard Morris <rwmorris.bjj@gmail.com>
2021-01-15 19:52:18 +0000

    working on practise problems

---
+++ javascript_language_fundamentals/javascript_basics/7.js
@@ -0,0 +1,19 @@
+// For this exercise we're going to learn more about type conversion by implementing our own parseInt function that 
+// converts a string of numeric characters (including an optional plus or minus sign) to a number.
+
+// The function takes a string of digits as an argument, and returns the appropriate number. Do not use any of the 
+// built-in functions for converting a string to a number type.
+
+// For now, do not worry about leading + or - signs, nor should you worry about invalid characters; assume all 
+// characters will be numeric.
+
+// Examples:
+
+function stringToInteger(string) {
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  
+  
+}
+
+stringToInteger('4321');      // 4321
+stringToInteger('570');       // 570
